0d719226a22cdb637b15bb4f84e33909acac9f77
huy nguyen <you@example.com>
2022-02-20 18:40:03 +0700

    up

--- module2/src/ss3_mang_va_phuong_thuc_java/thuc_hanh/TimGiaTriNhoNhatTrongMang.java
+++ module2/src/ss3_mang_va_phuong_thuc_java/thuc_hanh/TimGiaTriNhoNhatTrongMang.java
@@ -1,3 +1,18 @@
 package ss3_mang_va_phuong_thuc_java.thuc_hanh;
 
+public class TimGiaTriNhoNhatTrongMang {
+    public static void main(String[] args) {
+        int[] arr = {4, 12, 7, -8, 1, 6, 9};
+        System.out.println("Giá trị nhỏ nhất trong mảng là : " + minValue(arr));
+    }
 
+    public static int minValue(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}
---
+++ module2/src/ss3_mang_va_phuong_thuc_java/thuc_hanh/UngDungDemSoLuongSinhVienThiDo.java
@@ -0,0 +1,29 @@
+package ss3_mang_va_phuong_thuc_java.thuc_hanh;
+import java.util.Scanner;
+public class UngDungDemSoLuongSinhVienThiDo {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 30)
+                System.out.println("Size should not exceed 30");
+        } while (size > 30);
+        array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Enter a mark for student " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+        int count = 0;
+        System.out.println("List of mark: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+            if (array[i] >= 5 && array[i] <= 10) {
+                count++;
+            }
+        }
+        System.out.print("\nThe number of students passing the exam is " + count);
+    }
+}
