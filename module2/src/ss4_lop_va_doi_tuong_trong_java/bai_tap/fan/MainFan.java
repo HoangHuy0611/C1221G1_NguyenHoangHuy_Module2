2cd77268abceaa7ffd3f655252eb875065ef7429
huy nguyen <you@example.com>
2022-02-21 19:15:04 +0700

    up

---
+++ module2/src/ss4_lop_va_doi_tuong_trong_java/bai_tap/fan/MainFan.java
@@ -0,0 +1,14 @@
+package ss4_lop_va_doi_tuong_trong_java.bai_tap.fan;
+
+import ss4_lop_va_doi_tuong_trong_java.bai_tap.fan.Fan;
+
+public class MainFan {
+    public static void main(String[] args) {
+        Fan fan = new Fan();
+        Fan fan1 = new Fan(3, "on", 10, "yellow");
+        Fan fan2 = new Fan(2, "off", 5, "blue");
+        System.out.println(fan);
+        System.out.println(fan1);
+        System.out.println(fan2);
+    }
+}
---
+++ module2/src/ss4_lop_va_doi_tuong_trong_java/bai_tap/stop_watch/MainStopWatch.java
@@ -0,0 +1,34 @@
+package ss4_lop_va_doi_tuong_trong_java.bai_tap.stop_watch;
+
+import java.util.Random;
+public class MainStopWatch {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int size = 100000;
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand.nextInt(size);
+        }
+        StopWatch stopWatch = new StopWatch();
+        System.out.println("Start time (milliseconds): " + stopWatch.getStartTime());
+        stopWatch.start();
+
+        selectionSort(arr, size);
+
+        System.out.println("End time (milliseconds): " + stopWatch.getEndTime());
+        stopWatch.end();
+        System.out.println("Total (milliseconds): " + stopWatch.getElapsedTime() + " ~ " + stopWatch.getElapsedTime() / 1000 + " seconds");
+
+    }
+    public static void selectionSort(int[] arr, int size) {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[j] < arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
